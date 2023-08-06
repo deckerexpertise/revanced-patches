7108883ba8c728aa1c1145c97918671d717a5959
DeckerExpert <76219178+DeckerExpert@users.noreply.github.com>
2023-08-06 09:21:41 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,16 +33,51 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     // Required for FlexVer-Java
     implementation("com.unascribed:flexver-java:1.0.2")
+
     // A dependency to the Android library unfortunately fails the build,
     // which is why this is required for the patch change-oauth-client-id
     compileOnly(project("dummy"))
 }
+
 kotlin {
     jvmToolchain(11)
 }
+
 tasks {
     register<DefaultTask>("generateBundle") {
         description = "Generate dex files from build and bundle them in the jar file"
-        dependsOn(build)erateMeta"))    }
-}
+        dependsOn(build)
 
+        doLast {
+            val androidHome = System.getenv("ANDROID_HOME") ?: throw GradleException("ANDROID_HOME not found")
+            val d8 = "${androidHome}/build-tools/33.0.1/d8"
+            val input = configurations.archives.get().allArtifacts.files.files.first().absolutePath
+            val work = File("${buildDir}/libs")
+
+            exec {
+                workingDir = work
+                commandLine = listOf(d8, input)
+            }
+
+            exec {
+                workingDir = work
+                commandLine = listOf("zip", "-u", input, "classes.dex")
+            }
+        }
+    }
+    register<JavaExec>("generateMeta") {
+        description = "Generate metadata for this bundle"
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.revanced.meta.PatchesFileGenerator")
+    }
+    // Dummy task to fix the Gradle semantic-release plugin.
+    // Remove this if you forked it to support building only.
+    // Tracking issue: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
+    register<DefaultTask>("publish") {
+        group = "publish"
+        description = "Dummy task"
+        dependsOn(named("generateBundle"), named("generateMeta"))
+    }
+}
